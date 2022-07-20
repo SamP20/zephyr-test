6def2c881181dc6934f135971ebc2d385b7567cf
Sam <633856+SamP20@users.noreply.github.com>
2022-07-20 15:40:03 +0000

    Change app alias name

--- applications/my_app/module.mk
+++ applications/my_app/module.mk
@@ -1,10 +1,10 @@
 SCRIPT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-ZEPHYR_APP_ALIASES += board_app_alias
+ZEPHYR_APP_ALIASES += my_alias
 
 # Final application output
 # This could also depend on other outputs such as signed images
-PHONY: board_app_alias
-board_app_alias: $(BUILD_DIR)board_app_alias/zephyr/zephyr.bin
-board_app_alias: ZEPHYR_BOARD=nucleo_h753zi
-board_app_alias: ZEPHYR_APP_NAME=my_app
+PHONY: my_alias
+my_alias: $(BUILD_DIR)my_alias/zephyr/zephyr.bin
+my_alias: ZEPHYR_BOARD=nucleo_h753zi
+my_alias: ZEPHYR_APP_NAME=my_app
